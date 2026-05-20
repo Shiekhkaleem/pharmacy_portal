a334135e0b7eb587533d46ea89b399e9953b2e69
Kaleem Rehman <kaleem@blinkitech.com>
2026-05-21 00:58:49 +0500

    url updated

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001',
+    baseURL: import.meta.env.VITE_API_URL || 'https://pharmacyportalpro.vercel.app',
 });
 
 // Add a request interceptor
